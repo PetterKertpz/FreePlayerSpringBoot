b5eaa718453812834156e908ee6c8c5bb807fff9
PetterKertpz <petterlopez103@gmail.com>
2025-07-08 16:22:43 -0500

    Connfiguración inicial SpringBoot

---
+++ src/main/java/PMK/free_player/FreePlayerApplication.java
@@ -0,0 +1,14 @@
+package PMK.free_player;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreePlayerApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(FreePlayerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/PMK/free_player/FreePlayerApplicationTests.java
@@ -0,0 +1,13 @@
+package PMK.free_player;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreePlayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
