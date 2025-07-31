61304c4ce67667b23fa5941ab71ff4552106067b
PetterKertpz <petterlopez103@gmail.com>
2025-07-31 16:43:17 -0500

    Reapply "Sistema completado V1"
    This reverts commit 9eddab62ee06f71eeb1082a963d6fc4e0f69a043.
    
    # Conflicts:
    #	pom.xml
    #	src/main/java/PMK/free_player/FreePlayerApplication.java
    #	src/main/java/PMK/free_player/repositorys/AlbumRepositorio.java
    #	src/main/java/PMK/free_player/services/AlbumServicio.java

--- src/main/java/PMK/free_player/services/AlbumServicio.java
+++ src/main/java/PMK/free_player/services/AlbumServicio.java
@@ -32,6 +32,7 @@ public class AlbumServicio implements IAlbum {
     @Override
     public List<Album> listarAlbumesPorArtista(Integer idArtista) {
         log.info("Iniciando lista de albumes por artista");
+        List<Album> albumes = albumRepositorio.findAllByIdArtista(idArtista);
         if (albumes.isEmpty()) {
             log.error("Album no encontrado para el artista con ID: {}", idArtista);
             throw new RuntimeException("No se encontraron albumes para el artista con ID: " + idArtista);
@@ -56,6 +57,7 @@ public class AlbumServicio implements IAlbum {
     @Override
     public Optional<Album> findAlbumPorArtista(Integer idArtista) {
         log.info("Iniciando el album con ID {}", idArtista);
+        Optional<Album> albumes = albumRepositorio.findByIdArtista(idArtista);
         if (albumes.isEmpty()) {
             log.error("Album no encontrado para el artista con ID: {}", idArtista);
             throw new RuntimeException("No se encontró el album para el artista con ID: " + idArtista);
